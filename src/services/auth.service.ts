44abf5ed2e2a18b8dfb19a15ea788a0923647117
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-10-22 13:53:31 -0300

    feat: implementa autenticação JWT (registro e login)

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -14,7 +14,7 @@ export async function loginUser(email:string, password: string) {
     const user = await prisma.user.findUnique({where: {email}});
     if (!user) throw new Error("Credenciais inválidas");
     const ok = await bcrypt.compare(password, user.password);
-    if (!ok) therow new Error("Credenciais inválidas");
+    if (!ok) throw new Error("Credenciais inválidas");
 
     const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET!, {expiresIN: "7d"});
     return { token, user: { id: user.id, name: user.name, email: user.email}};
