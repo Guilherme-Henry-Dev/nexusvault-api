1050a66b3b4bfcb78134f1a70e36f8f8a056303a
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-10-22 14:26:25 -0300

    feat: implementa autenticação JWT (registro e login)

---
+++ src/controllers/auth.controllers.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { registerSchema, loginSchema } from "../validations/auth.schema";
+import { loginUser, registerUser } from "../services/auth.service";
+import { console } from "inspector";
+
+export async function register(req:Request, res: Response) {
+    const parsed = registerSchema.safeParse(req.body);
+    if (!parsed.success) return res.status(400).json(parsed.error.format())
+        const {name, email, password} = parsed.data;
+    try {
+        const user = await registerUser(name, email, password);
+        res.status(201).json({ id: user.id, name: user.name, email: user.email })
+    } catch (e: any) {
+        res.status(400).json({ error: e.message })
+    }
+}
+
+export async function login(req:Request, res: Response) {
+    const parsed = loginSchema.safeParse(req.body);
+    if (!parsed.sucess) return res.status(400).json(parsed.error.format());
+        const { email, password } = parsed.data;
+    try {
+        const result = await loginUser(email, password);
+        res.json(result);
+    } catch (e: any) {
+        res.status(400).json({ error: e.message})
+    }
+}
