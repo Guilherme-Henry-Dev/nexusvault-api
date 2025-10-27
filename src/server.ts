e8d1149e92cfcb2da56457b7fdf51c294a7ef55c
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-10-27 09:28:12 -0300

    docs: Criado a documentações Swagger da API

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,11 @@ import { authRouter } from './routes/auth.routes.js';
 import  gamesRouter  from './routes/games.routes.js';
 import { errorHandler } from "./middlewares/errorHandler.js";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./docs/swagger.json" assert { type: "json" };
 
 const app = express();
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"));
